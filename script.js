f36c5d4489f6fa8275306cf1138eacfc11c2a396
mhmtalakus <mehmetalakus02@icloud.com>
2025-01-08 13:55:52 +0300

    Dynamic display of educations has been enabled.

--- script.js
+++ script.js
@@ -85,3 +85,23 @@ fetch("./data.json")
             workElement.innerHTML += worksHTML;
         })
     })
+
+const educationElement = document.querySelector("#educations");
+// console.log(educationElement);
+
+fetch("./data.json")
+.then(response => response.json())
+.then(data => {
+    let educationHTML = "";
+    data.educations.forEach((item, index) => {
+        educationHTML = `
+        <div class="w3-container">
+            <h5 class="w3-opacity"><b>${item.institution}</b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>${item.startDate} - ${item.endDate}</h6>
+            <p>${item.degree}</p>
+            ${index < data.educations.length - 1 ? '<hr>' : ""}
+        </div>
+        `;
+        educationElement.innerHTML += educationHTML;
+    })
+})
